99b4169c9e19525bb051d81dff90536d47fccc29
Prakruthi Manjunath <44273409+prakruthi3@users.noreply.github.com>
2020-04-05 23:40:31 +0530

    Updated Dem

--- Demo.java
+++ Demo.java
@@ -1,31 +1,26 @@
 package com.tgt.igniteplus;
-
 //abstract class demonstration
-abstract class Sum{
-    /* These two are abstract methods, the child class
-     * must implement these methods
-     */
+abstract class Sum
+{
     public abstract int sumOfTwo(int n1, int n2);
     public abstract int sumOfThree(int n1, int n2, int n3);
-
-    //Regular method
-    public void disp(){
+    public void disp()
+    {
         System.out.println("Method of class Sum");
     }
 }
-//Regular class extends abstract class
-class Demo extends Sum{
-
-    /* If I don't provide the implementation of these two methods, the
-     * program will throw compilation error.
-     */
-    public int sumOfTwo(int num1, int num2){
+class Demo extends Sum
+{
+    public int sumOfTwo(int num1, int num2)
+    {
         return num1+num2;
     }
-    public int sumOfThree(int num1, int num2, int num3){
+    public int sumOfThree(int num1, int num2, int num3)
+    {
         return num1+num2+num3;
     }
-    public static void main(String args[]){
+    public static void main(String args[])
+    {
         Sum obj = new Demo();
         System.out.println(obj.sumOfTwo(3, 7));
         System.out.println(obj.sumOfThree(4, 3, 19));
